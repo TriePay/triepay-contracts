04d5e94e7c7b246ab522f3749ae31035833e6e2d
liangwu <liangwucas@gmail.com>
2022-06-16 13:07:45 +0800

    network config for ethereum/bsc/polygon/avalanche/fantom etc.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,13 +23,33 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.7",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    ethereum: {
+      url: process.env.ETHEREUM_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    bsc: {
+      url: process.env.BSC_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    polygon: {
+      url: process.env.POLYGON_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    avalanche: {
+      url: process.env.AVALANCHE_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    fantom: {
+      url: process.env.FANTOM_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    }
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
